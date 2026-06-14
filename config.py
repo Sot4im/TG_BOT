ca50b1ef4884e5ea86b9cb1b13c0381b16cb32d5
Sot4im <mitackindanil@gmail.com>
2026-06-14 16:23:28 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,8 +1,8 @@
 import os
 
-TOKEN = 
+TOKEN ="8505103955:AAEUrj6yjqHn6HzIC2pAxiWLDNLVJIMTN0c"
 
-ADMIN_ID =
+ADMIN_ID =2141337014
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 MEDIA_DIR = os.path.join(BASE_DIR, 'media')
